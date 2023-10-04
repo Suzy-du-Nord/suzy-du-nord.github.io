804b7d78a1bf2a0f4785acf1fd201f5567dca06a
Ryan Iem <ryan.iem12@gmail.com>
2023-10-04 20:07:30 +0900

    new ideas

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let defaultTopicSelection = 1
+
+// alert("hehe");
+document.getElementById("profile-topic-" + defaultTopicSelection).style = "text-decoration: underline";
+
+function changeTopic(selection) {
+    for (i = 1; i <= 4; i++) {
+        document.getElementById("profile-topic-" + i).style = "text-decoration: none";
+        document.getElementById("profile-topic-" + i).style = "cursor: pointer";
+        document.getElementById("profile-topic-content-" + i).style = "display: none";
+    }
+    document.getElementById("profile-topic-" + selection).style = "text-decoration: underline; cursor: pointer;";
+    document.getElementById("profile-topic-content-" + selection).style = "display: ''";
+}
