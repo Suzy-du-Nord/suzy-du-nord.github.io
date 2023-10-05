16de01603ca2c7a4216db4e4cf3b72952c007014
Ryan Iem <ryan.iem12@gmail.com>
2023-10-06 01:11:26 +0900

    smooth scrolling

--- js/index.js
+++ js/index.js
@@ -5,6 +5,16 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});
+
 function setup() {
     for (i = 1; i <= 4; i++) {
         document.getElementById("profile-topic-" + i).style = "cursor: pointer";
